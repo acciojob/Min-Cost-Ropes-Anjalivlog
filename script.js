48f3c51a2a94d927af8934af0eef803ff65cca2e
Anjalivlog <139326744+Anjalivlog@users.noreply.github.com>
2024-04-02 17:30:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,17 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+	if (arr.length == 0) return 0;
+	let totalCost = 0;
+	while (arr.length > 1) {
+		arr.sort((a,b) => a - b);
+		const min1 = arr.shift();
+		const min2 = arr.shift();
+		const combineLength = min1 + min2;
+		totalCost += combineLength;
+		arr.push(combineLength);
+	}
+	return totalCost;
 }
 
 module.exports=mincost;
